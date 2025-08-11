fc2c5229429d1fcf96a17930dfd10ab0829516ba
Pablo Estrada <pablojoseestradareyes@gmail.com>
2025-08-10 22:57:03 -0600

    nueva linea

--- probando/Program.cs
+++ probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Platzi!");
 
 string stripe_api_key = "sk_test_PrU3B44M1n";
+
+string testingConnectionString = "Server=localhost;Port=1111;Database=platzi;User Id=postgres;Password=12345678;";
