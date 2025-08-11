af4960aa2041d59bea7ce9090f5b24b1163dec4a
Pablo Estrada <pablojoseestradareyes@gmail.com>
2025-08-10 22:55:53 -0600

    actualizacion llave

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key = "sk_test_51N0bX2LhYk1b3KX8vYtX";
+string stripe_api_key = "sk_test_PrU3B44M1n";
